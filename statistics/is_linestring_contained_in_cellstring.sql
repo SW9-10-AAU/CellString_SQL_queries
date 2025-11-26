7786a10618f460be187409367edae29d848877fa
Jonas Geertsen Lund <jgl21@student.aau.dk>
2025-11-26 14:31:29 +0100

    Use CST_AsMultiPolygon instead of draw_cellstring

--- statistics/is_linestring_contained_in_cellstring.sql
+++ statistics/is_linestring_contained_in_cellstring.sql
@@ -1,9 +1,9 @@
 -- Bresenham
 SELECT
     ls.trajectory_id,
-    ST_Contains(draw_cellstring(cs.cellstring_z13, 13), ls.geom) AS z13_contains,
-    ST_Contains(draw_cellstring(cs.cellstring_z17, 17), ls.geom) AS z17_contains,
-    ST_Contains(draw_cellstring(cs.cellstring_z21, 21), ls.geom) AS z21_contains
+    ST_Contains(CST_AsMultiPolygon(cs.cellstring_z13, 13), ls.geom) AS z13_contains,
+    ST_Contains(CST_AsMultiPolygon(cs.cellstring_z17, 17), ls.geom) AS z17_contains,
+    ST_Contains(CST_AsMultiPolygon(cs.cellstring_z21, 21), ls.geom) AS z21_contains
 FROM prototype2.trajectory_ls ls
 JOIN prototype2.trajectory_cs cs
     ON ls.trajectory_id = cs.trajectory_id
@@ -12,9 +12,9 @@ WHERE ls.trajectory_id = 8403;
 -- Supercover
 SELECT
     ls.trajectory_id,
-    ST_Contains(draw_cellstring(cs.cellstring_z13, 13), ls.geom) AS z13_contains,
-    ST_Contains(draw_cellstring(cs.cellstring_z17, 17), ls.geom) AS z17_contains,
-    ST_Contains(draw_cellstring(cs.cellstring_z21, 21), ls.geom) AS z21_contains
+    ST_Contains(CST_AsMultiPolygon(cs.cellstring_z13, 13), ls.geom) AS z13_contains,
+    ST_Contains(CST_AsMultiPolygon(cs.cellstring_z17, 17), ls.geom) AS z17_contains,
+    ST_Contains(CST_AsMultiPolygon(cs.cellstring_z21, 21), ls.geom) AS z21_contains
 FROM prototype2.trajectory_ls ls
 JOIN prototype2.trajectory_supercover_cs cs
     ON ls.trajectory_id = cs.trajectory_id
@@ -24,9 +24,9 @@ WHERE ls.trajectory_id = 8403;
 -- Visualisation
 SELECT
     ls.geom AS trajectory_geom,
-    draw_cellstring(cs.cellstring_z13, 13) AS cells_z13,
-    draw_cellstring(cs.cellstring_z17, 17) AS cells_z17,
-    draw_cellstring(cs.cellstring_z21, 21) AS cells_z21
+    CST_AsMultiPolygon(cs.cellstring_z13, 13) AS cells_z13,
+    CST_AsMultiPolygon(cs.cellstring_z17, 17) AS cells_z17,
+    CST_AsMultiPolygon(cs.cellstring_z21, 21) AS cells_z21
 FROM prototype2.trajectory_ls ls
 JOIN prototype2.trajectory_cs cs
 -- JOIN prototype2.trajectory_supercover_cs cs
--- visualisations/visualise_cellstring_on_diff_zooms.sql
+++ visualisations/visualise_cellstring_on_diff_zooms.sql
@@ -1,11 +1,11 @@
 -- ==========================================================
--- Test trajectory query using draw_cellstring (zoom 21)
+-- Test trajectory query using CST_AsMultiPolygon (zoom 21)
 -- ==========================================================
 SELECT
     ls.trajectory_id,
     ls.mmsi,
     ls.geom               AS trajectory_geom,
-    draw_cellstring(cs.cellstring_z21, 21) AS trajectory_cells
+    CST_AsMultiPolygon(cs.cellstring_z21, 21) AS trajectory_cells
 FROM prototype2.trajectory_ls ls
 JOIN prototype2.trajectory_cs cs
     ON ls.trajectory_id = cs.trajectory_id
@@ -13,13 +13,13 @@ WHERE ls.trajectory_id IN (8403);
 
 
 -- ==========================================================
--- Test trajectory query using draw_cellstring (zoom 17)
+-- Test trajectory query using CST_AsMultiPolygon (zoom 17)
 -- ==========================================================
 SELECT
     ls.trajectory_id,
     ls.mmsi,
     ls.geom               AS trajectory_geom,
-    draw_cellstring(cs.cellstring_z17, 17) AS trajectory_cells
+    CST_AsMultiPolygon(cs.cellstring_z17, 17) AS trajectory_cells
 FROM prototype2.trajectory_ls ls
 JOIN prototype2.trajectory_cs cs
     ON ls.trajectory_id = cs.trajectory_id
@@ -27,13 +27,13 @@ WHERE ls.trajectory_id IN (8403);
 
 
 -- ==========================================================
--- Test trajectory query using draw_cellstring (zoom 13)
+-- Test trajectory query using CST_AsMultiPolygon (zoom 13)
 -- ==========================================================
 SELECT
     ls.trajectory_id,
     ls.mmsi,
     ls.geom               AS trajectory_geom,
-    draw_cellstring(cs.cellstring_z13, 13) AS trajectory_cells
+    CST_AsMultiPolygon(cs.cellstring_z13, 13) AS trajectory_cells
 FROM prototype2.trajectory_ls ls
 JOIN prototype2.trajectory_cs cs
     ON ls.trajectory_id = cs.trajectory_id
@@ -47,9 +47,9 @@ SELECT
     ls.trajectory_id,
     ls.mmsi,
     ls.geom AS trajectory_geom,
-    draw_cellstring(cs.cellstring_z13, 13) AS cells_z13,
-    draw_cellstring(cs.cellstring_z17, 17) AS cells_z17,
-    draw_cellstring(cs.cellstring_z21, 21) AS cells_z21
+    CST_AsMultiPolygon(cs.cellstring_z13, 13) AS cells_z13,
+    CST_AsMultiPolygon(cs.cellstring_z17, 17) AS cells_z17,
+    CST_AsMultiPolygon(cs.cellstring_z21, 21) AS cells_z21
 FROM prototype2.trajectory_ls ls
 JOIN prototype2.trajectory_cs cs
     ON ls.trajectory_id = cs.trajectory_id
@@ -57,13 +57,13 @@ WHERE ls.trajectory_id IN (8403);
 
 
 -- ==========================================================
--- Test stop query using draw_cellstring (zoom 21)
+-- Test stop query using CST_AsMultiPolygon (zoom 21)
 -- ==========================================================
 SELECT
     ls.stop_id,
     ls.mmsi,
     ls.geom               AS stop_geom,
-    draw_cellstring(cs.cellstring_z21, 21) AS stop_cells
+    CST_AsMultiPolygon(cs.cellstring_z21, 21) AS stop_cells
 FROM prototype2.stop_poly ls
 JOIN prototype2.stop_cs cs
     ON ls.stop_id = cs.stop_id
@@ -71,13 +71,13 @@ WHERE ls.stop_id IN (5291);
 
 
 -- ==========================================================
--- Test stop query using draw_cellstring (zoom 17)
+-- Test stop query using CST_AsMultiPolygon (zoom 17)
 -- ==========================================================
 SELECT
     ls.stop_id,
     ls.mmsi,
     ls.geom               AS stop_geom,
-    draw_cellstring(cs.cellstring_z17, 17) AS stop_cells
+    CST_AsMultiPolygon(cs.cellstring_z17, 17) AS stop_cells
 FROM prototype2.stop_poly ls
 JOIN prototype2.stop_cs cs
     ON ls.stop_id = cs.stop_id
@@ -85,13 +85,13 @@ WHERE ls.stop_id IN (5291);
 
 
 -- ==========================================================
--- Test stop query using draw_cellstring (zoom 13)
+-- Test stop query using CST_AsMultiPolygon (zoom 13)
 -- ==========================================================
 SELECT
     ls.stop_id,
     ls.mmsi,
     ls.geom               AS stop_geom,
-    draw_cellstring(cs.cellstring_z13, 13) AS stop_cells
+    CST_AsMultiPolygon(cs.cellstring_z13, 13) AS stop_cells
 FROM prototype2.stop_poly ls
 JOIN prototype2.stop_cs cs
     ON ls.stop_id = cs.stop_id
@@ -104,9 +104,9 @@ SELECT
     ls.stop_id,
     ls.mmsi,
     ls.geom AS stop_geom,
-    draw_cellstring(cs.cellstring_z13, 13) AS cells_z13,
-    draw_cellstring(cs.cellstring_z17, 17) AS cells_z17,
-    draw_cellstring(cs.cellstring_z21, 21) AS cells_z21
+    CST_AsMultiPolygon(cs.cellstring_z13, 13) AS cells_z13,
+    CST_AsMultiPolygon(cs.cellstring_z17, 17) AS cells_z17,
+    CST_AsMultiPolygon(cs.cellstring_z21, 21) AS cells_z21
 FROM prototype2.stop_poly ls
 JOIN prototype2.stop_cs cs
     ON ls.stop_id = cs.stop_id
