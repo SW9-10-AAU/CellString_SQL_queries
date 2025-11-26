13442371b378c9a475272f0a479d5f98e34ffa1e
Jonas Geertsen Lund <jgl21@student.aau.dk>
2025-11-26 16:02:53 +0100

    Added CST_Coverage and CST_Coverage_ByMMSI

--- cellstring_specific_queries/coverage_of_area_by_mmsi.sql
+++ cellstring_specific_queries/coverage_of_area_by_mmsi.sql
@@ -1,5 +1,14 @@
 -- Coverage of area (%) by mmsi
 
+SELECT *
+FROM CST_Coverage_ByMMSI(
+  'prototype2.trajectory_supercover_cs',
+  17,
+  (SELECT cellstring_z17 FROM benchmark.area_cs WHERE area_id = 3)
+);
+
+
+-- Alternatively, using the following explicit SQL:
 -- Hals-Egense (0.8s)
 -- Helsingør-Helsingborg (23s)
 -- Læsø (????)
@@ -13,7 +22,7 @@ traj_cover AS (
         traj.mmsi,
         CST_Intersection(traj.cellstring_z17, area.cellstring_z17) AS covered_cells
     FROM
-        prototype2.trajectory_cs AS traj,
+        prototype2.trajectory_supercover_cs AS traj,
         area
     WHERE
         CST_Intersects(traj.cellstring_z17, area.cellstring_z17)
@@ -21,13 +30,12 @@ traj_cover AS (
 mmsi_union AS (
     SELECT
         mmsi,
-        CST_Union_Agg(covered_cells) AS union_cells,
-        cardinality(CST_Union_Agg(covered_cells)) AS cell_count
+        CST_Union_Agg(covered_cells) AS union_cells
     FROM traj_cover
     GROUP BY mmsi
 )
 SELECT
     mmsi,
-    ROUND((cell_count::numeric / cardinality(area.cellstring_z17)) * 100, 2) AS coverage_percent
+    CST_Coverage(union_cells, area.cellstring_z17) AS coverage_percent
 FROM mmsi_union, area
 ORDER BY coverage_percent DESC;
--- cellstring_specific_queries/coverage_of_area_by_traj_id.sql
+++ cellstring_specific_queries/coverage_of_area_by_traj_id.sql
@@ -3,15 +3,11 @@
 SELECT
     traj.mmsi,
     traj.trajectory_id,
-    ROUND(
-        (cardinality(CST_Intersection(traj.cellstring_z21, area.cellstring_z21))::numeric
-         / cardinality(area.cellstring_z21)) * 100,
-        2
-    ) AS coverage_percent
+    CST_Coverage(traj.cellstring_z17, area.cellstring_z17) AS coverage_percent
 FROM
     prototype2.trajectory_cs AS traj,
     benchmark.area_cs as area
 WHERE
     area.area_id = 3
-    AND CST_Intersects(traj.cellstring_z21, area.cellstring_z21)
+    AND CST_Intersects(traj.cellstring_z17, area.cellstring_z17)
 ORDER BY coverage_percent DESC;
