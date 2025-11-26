69a743059e16f5247e77205a29e438c1b8adb2e7
Jonas Geertsen Lund <jgl21@student.aau.dk>
2025-11-26 14:22:37 +0100

    use z17 in coverage

--- cellstring_specific_queries/coverage_of_area_by_mmsi.sql
+++ cellstring_specific_queries/coverage_of_area_by_mmsi.sql
@@ -4,19 +4,19 @@
 -- Helsingør-Helsingborg (23s)
 -- Læsø (????)
 WITH area AS (
-    SELECT cellstring_z21
+    SELECT cellstring_z17
     FROM benchmark.area_cs
     WHERE area_id = 3
 ),
 traj_cover AS (
     SELECT
         traj.mmsi,
-        CST_Intersection(traj.cellstring_z21, area.cellstring_z21) AS covered_cells
+        CST_Intersection(traj.cellstring_z17, area.cellstring_z17) AS covered_cells
     FROM
         prototype2.trajectory_cs AS traj,
         area
     WHERE
-        CST_Intersects(traj.cellstring_z21, area.cellstring_z21)
+        CST_Intersects(traj.cellstring_z17, area.cellstring_z17)
 ),
 mmsi_union AS (
     SELECT
@@ -28,6 +28,6 @@ mmsi_union AS (
 )
 SELECT
     mmsi,
-    ROUND((cell_count::numeric / cardinality(area.cellstring_z21)) * 100, 2) AS coverage_percent
+    ROUND((cell_count::numeric / cardinality(area.cellstring_z17)) * 100, 2) AS coverage_percent
 FROM mmsi_union, area
 ORDER BY coverage_percent DESC;
